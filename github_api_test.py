bc8ec34480f6cf2efecbb1ce11614d744b3af278
Mauricio Pilla <pilla@inf.ufpel.edu.br>
2015-07-07 14:29:39 -0300

    testing

---
+++ github_api_test.py
@@ -0,0 +1,11 @@
+import json, requests
+
+url = 'https://github.com/repos/pilla/try_git/hooks'
+
+params = ''
+
+resp = requests.get(url=url)
+# data = json.loads(resp.text)
+
+print (resp.text)
+# print (data)
